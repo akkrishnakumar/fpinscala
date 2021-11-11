9f9342c52a989cb6277c7af11df260364ce1c41a
Akhil Krishnakumar <akhil.krishnakumar.iicmr@gmail.com>
2021-11-11 15:46:06 +0100

    Create Chapter8 Exercise Spec

---
+++ src/test/scala/chapter8/Chapter8ExercisesSpec.scala
@@ -0,0 +1,17 @@
+package chapter8
+
+import utils.BaseSpec
+
+class Chapter8ExercisesSpec extends BaseSpec {
+
+  test("Ex 8.1 - Write down properties for method sum: List[Int] => Int") {
+    // Abstract
+  }
+
+  test("Ex 8.2 - Write down properties for max of List[Int]") {
+    // Abstract
+  }
+
+  
+
+}
